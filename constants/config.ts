9839642e289cc36db7a04f6af8ae0e9fa1a1c30a
dominik <w1778659@my.westminster.ac.uk>
2024-04-27 17:39:24 +0100

    added render API

--- constants/config.ts
+++ constants/config.ts
@@ -1,7 +1,7 @@
 /**
  * Server domain.
- * Set to local IP address if server is on localhost.
+ * NOTE: if server is running on localhost, set API to local IP address (see commented out line), NOT 'localhost:port'
  * Source: https://stackoverflow.com/questions/47417766/calling-locally-hosted-server-from-expo-app
  */
-export const API = 'http://192.168.1.10:3000';
-// export const API = 'http://localhost:3000';
+// export const API = 'http://192.168.1.10:3000';
+export const API = 'https://exertrack-server.onrender.com';
